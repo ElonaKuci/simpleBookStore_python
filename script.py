c92a70cf184d485f994aea0e9502a340ee84253f
ElonaKuci <37561804+ElonaKuci@users.noreply.github.com>
2020-09-14 03:11:37 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,46 @@
 from tkinter import *
+import backend
+
+
+def get_selected_row(event):
+  try:
+    global selected_tuple
+    index=list1.curselection()[0]
+    selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
+  except IndexError:
+        pass
+
+
+
+def view_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END,row )
+
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(title_value.get(),author_value.get(),year_value.get(),isbn_value.get()):
+        list1.insert(END,row)
+
+def insert_command():
+    backend.insert(title_value.get(),author_value.get(),year_value.get(),isbn_value.get())
+    list1.delete(0,END)
+    list1.insert(END,(title_value.get(),author_value.get(),year_value.get(),isbn_value.get()))
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+
+def update_command():
+    backend.update(selected_tuple[0],title_value.get(),author_value.get(),year_value.get(),isbn_value.get())
+
 
 window= Tk()
 
@@ -29,22 +71,22 @@ e4=Entry(window, textvariable=isbn_value)
 e4.grid(row=1,column=3)
 
 
-b1=Button(window,text="View All",command="",width="15")
+b1=Button(window,text="View All",command=view_command,width="15")
 b1.grid(row=3,column=3)
 
-b2=Button(window,text="Search Entry",command="",width="15")
+b2=Button(window,text="Search Entry",command=search_command,width="15")
 b2.grid(row=4,column=3)
 
-b3=Button(window,text="Add Entry",command="",width="15")
+b3=Button(window,text="Add Entry",command=insert_command,width="15")
 b3.grid(row=5,column=3)
 
-b4=Button(window,text="Update Selected",command="",width="15")
+b4=Button(window,text="Update Selected",command=update_command,width="15")
 b4.grid(row=6,column=3)
 
-b5=Button(window,text="Delete Selected",command="",width=15)
+b5=Button(window,text="Delete Selected",command=delete_command,width=15)
 b5.grid(row=7,column=3)
 
-b6=Button(window,text="Close",command="",width=15)
+b6=Button(window,text="Close",command=window.destroy,width=15)
 b6.grid(row=8,column=3)
 
 list1= Listbox(window,height=6,width=35)
@@ -54,5 +96,6 @@ sb1=Scrollbar(window)
 sb1.grid(row=2,column=2,rowspan=6)
 #list1.configure(yscrollcommand=sb1.set)
 #sb1.configure(command=list1.yview)
-
+list1.bind('<<ListboxSelect>>',get_selected_row)
+window.wm_title("BookStore")
 window.mainloop()
